68c968d5f70e51b7cd80c0bd912c80ec59fbdd6e
lvntoguz <lvntoguz@gmail.com>
2020-08-12 22:01:24 +0300

    ad
    :wq
    :aq
    P
    P

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const harflerTurkce= ["A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z"];
 
 const btnYeniOyun = document.getElementById("btnBasla");
+const grafik = document.getElementById("grafik");
 const harfler = Array.from(document.getElementsByClassName("harfBtn"));
 const soruContainer = document.getElementById("soru-container");
 
@@ -8,7 +9,6 @@ let soruKelimesi = "";
 let soruTahmin = "";
 let kullanilmisHarfler = "";
 let yanlisTahminSayisi = 0;
-
 
 const kelimeler = ["bardak", "çatal", "kaşık", "elma", "armut"];
 
@@ -36,15 +36,10 @@ harfler.forEach((harf) => {
             return;
         } else {    //Harf soruda yoksa
             yanlisTahminSayisi++;
-            console.log(yanlisTahminSayisi);
+            grafikCiz();
             //Grafik çiz
-
-
             //Hakları bittiyse oyunu bitir
-
-
         }
-
     });
 });
 
@@ -64,9 +59,38 @@ btnYeniOyun.addEventListener("click" , function(){
         soruTahmin += "_";
     }
     soruContainer.innerHTML = soruTahmin;
-    
+    grafikCiz();
     //Harfleri sıfırla
-
-
 })
 
+
+function grafikCiz(){
+    let grafikTag ='<svg width="200" height="250">';
+    grafikTag += '<line x1="10" y1="10" x2="10" y2="190" style="stroke:black;stroke-width:2" />';                              //taban direk uzatma
+    grafikTag += '<line x1="10" y1="10" x2="80" y2="10" style="stroke:black;stroke-width:2" />';
+    grafikTag += '<line x1="10" y1="190" x2="150" y2="190" style="stroke:black;stroke-width:2" />';
+    grafikTag += '<line x1="80" y1="10" x2="80" y2="40" style="stroke:black;stroke-width:2" />';
+    if (yanlisTahminSayisi > 0) { //kafa
+        grafikTag += '<circle cx="80" cy="50" r="10" stroke="red" stroke-width="3" fill="red" />';
+    };                
+    if (yanlisTahminSayisi > 1) { //gövde
+        grafikTag += '<line x1="80" y1="60" x2="80" y2="120" style="stroke:rgb(255,0,0);stroke-width:2" />';
+    };                
+    if (yanlisTahminSayisi > 2) { //kol1
+        grafikTag += '<line x1="80" y1="70" x2="60" y2="90" style="stroke:rgb(255,0,0);stroke-width:2" />';
+    };                
+    if (yanlisTahminSayisi > 3) { //kol2
+        grafikTag += '<line x1="80" y1="70" x2="100" y2="90" style="stroke:rgb(255,0,0);stroke-width:2" />';
+    };                
+    if (yanlisTahminSayisi > 4) { //ayak1
+        grafikTag += '<line x1="80" y1="120" x2="60" y2="140" style="stroke:rgb(255,0,0);stroke-width:2" />';
+    };                
+    if (yanlisTahminSayisi > 5) { //ayak2
+        grafikTag += '<line x1="80" y1="120" x2="100" y2="140" style="stroke:rgb(255,0,0);stroke-width:2" />';
+        grafikTag += '<text x="20" y="220" fill="red">KAYBETTİNİZ!!!</text>';
+    };                
+
+    grafikTag += "</svg>";
+    grafik.innerHTML = grafikTag;
+    return;
+}
